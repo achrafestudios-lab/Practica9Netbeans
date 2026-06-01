43144867521ac8ff6ea9e2ab94c2510f2d86a391
Achraf Ait <achrafestudios@gmail.com>
2026-06-01 18:24:22 +0200

    Version 1

---
+++ src/main/java/com/mycompany/practica9git/Practica9Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica9git;
+
+/**
+ *
+ * @author ach.dev
+ */
+public class Practica9Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
